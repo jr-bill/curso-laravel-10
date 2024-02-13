faacbfe2ba7ac00dac843fb362bab19df0360fcc
Bill Jr <jrbill@hackermail.com>
2024-02-13 14:05:40 -0300

    Services add

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,76 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
+use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function __construct(
+        protected SupportService $service
+    ) {}
+
+    public function index(Request $request)
     {
-        return view('admin/supports/index');
+        $supports = $this->service->getAll($request->filter);
+
+        return view('admin/supports/index', compact('supports'));
+    }
+
+    public function show(string $id)
+    {
+        // Support::find($id);
+        // Support::where('id', $id)->first();
+        // Support::where('id', '=', $id)->first();
+        if (!$support = $this->service->findOne($id)) {
+            return back();
+        }
+        
+        return view('admin/supports/show', compact('support'));
+    }
+
+    public function create()
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(StoreUpdateSupport $request, Support $support)
+    {
+        $data = $request->validated();
+        $data['status'] = 'a';
+
+        $support = $support->create($data);
+        
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(string $id) 
+    {
+        //if (!$support = $support->where('id', $id)->first()) {
+        if (!$support = $this->service->findOne($id)) {
+            return back();
+        }
+
+        return view('admin/supports/edit', compact('support'));
+    }
+
+    public function update(StoreUpdateSupport $request, Support $support, string $id)
+    {
+        if (!$support = $support->find($id)) {
+            return back();
+        }
+
+        $support->update($request->validated());
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string $id)
+    {
+        $this->service->delete($id)
+
+        return redirect()->route('supports.index');
     }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,25 @@
-<h1>Listagem dos suportes</h1>
+<h1>Listagem dos Suportes</h1>
+
+<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th>Ações</th>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    <a href="{{ route('supports.show', $support->id) }}">ir</a>
+                    <a href="{{ route('supports.edit', $support->id) }}">editar</a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
